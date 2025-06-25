2879489a16b81444e5426d25887a1e9f9bb7bfef
trung.la <laquangtrung1241997nd@gmail.com>
2025-06-25 23:44:18 +0700

    Implement Handler abstract class

---
+++ 7. Open source examples/kafka/userprofile-service/include/handlers/Handler.h
@@ -0,0 +1,35 @@
+/*
+* File: Handler.h
+* Author: trung.la
+* Date: 06-25-2025
+* Description: This file is declaration of Handler class
+*/
+
+#ifndef HANDLER_H
+#define HANDLER_H
+
+class Event;
+
+class Handler
+{
+public:
+    /**
+     * @brief Default constructor for Handler class
+     */
+    Handler() = default;
+
+    /**
+     * @brief Default destructor for Handler class
+     */
+    virtual ~Handler() = default;
+
+    /**
+     * @brief Handle event
+     * This method is intended to be overridden by derived classes to handle specific events.
+     * @param event The event to handle
+     * @return true if the event was handled successfully, false otherwise
+     */
+    virtual bool handleEvent(const Event& event) = 0;
+};
+
+#endif // HANDLER_H
